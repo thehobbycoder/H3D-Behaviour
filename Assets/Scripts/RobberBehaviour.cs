586c55c604e7b68a41a365538b751975c0925384
thehobbycoder <028941_thehobbycoder@users.noreply.github.com>
2021-10-31 08:07:42 +0800

    Behavior Tree Concepts - Tree Printing

---
+++ Assets/Scripts/RobberBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace HC_BehaviourTree
+{
+    public class RobberBehaviour : MonoBehaviour
+    {
+        BehaviourTree tree;
+        void Start()
+        {
+            tree = new BehaviourTree();
+            Node steal = new Node("Steal Something");
+            Node gotoDiamond = new Node("Go to Diamond");
+            Node gotoVan = new Node("Go to Van");
+
+            steal.AddChild(gotoDiamond);
+            steal.AddChild(gotoVan);
+            tree.AddChild(steal);
+
+            tree.PrintTree();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
